7ef0c419f872e981ab7c201a541b01d9e3b82f32
Nelson Junior <ircnelson@gmail.com>
2019-09-15 21:03:40 -0400

    Altera o nome da solution e projeto

--- src/FluentQuery.ConsoleSample/Program.cs
+++ src/FluentQuery.ConsoleSample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using static FluentQuery.Entities;
-namespace FluentQuery
+
+namespace FluenteQuery.ConsoleSample
 {
     class Program
     {
@@ -8,19 +9,19 @@ namespace FluentQuery
         {
             var uf = "MT";
             
-            var q = new FluentQuery();
+            var q = new FluentQuery.FluentQuery();
             var query = q
-                .Select(Pessoa.Nome)
+                    .Select(Pessoa.Nome)
                     .〡(Pessoa.Idade)
                     .〡(Funcionario.Salario * 200).As("Valor_Final")
                     .〡(Pessoa.Telefone).As("[Telefone Teste]")
                     .〡Sum(Funcionario.Salario).As("ValorSalario")
-                .From(Funcionario)
+                    .From(Funcionario)
                     .InnerJoin(Pessoa)  .On(Pessoa.PessoaId == Funcionario.PessoaId)
                     .LeftJoin(Endereco) .On〱〱(Endereco.EnderecoId == Pessoa.EnderecoId).And( Endereco.Situacao == "ATIVO").〱〱
                     .LeftJoin(Cidade)   .On(Cidade.CidadeId == Endereco.CidadeId)
                     .LeftJoin(Uf)       .On(Uf.UfId == Cidade.UfId)
-                .Where(Funcionario.Salario >= 2500.95)
+                    .Where(Funcionario.Salario >= 2500.95)
                     .And〱〱 (Pessoa.Idade > 18).Or(Pessoa.Sexo != "M").〱〱
                 ;
             // Dynamic Where Condition
@@ -28,9 +29,9 @@ namespace FluentQuery
                 q = q.And(Uf.Sigla == uf);
                     
             q = q.OrderBy〱〱
-                    .〡〡(Pessoa.Idade) .Desc
-                    .〡(Pessoa.PessoaId)
-                    .〱〱;
+                .〡〡(Pessoa.Idade) .Desc
+                .〡(Pessoa.PessoaId)
+                .〱〱;
             
             Console.WriteLine(q);
         }
